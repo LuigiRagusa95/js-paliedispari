afb25471b45a793404868559d41a2cfd3ded4f44
LuigiRagusa95 <luigiragusa99@gmail.com>
2021-10-29 14:56:53 +0200

    refactor and init second jsnack

--- js/main.js
+++ js/main.js
@@ -2,38 +2,39 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
-const input = document.getElementById('p-001');
-const button = document.getElementById('b-001');
+const inputPalindrome = document.getElementById('p-001');
+const buttonPalindrome = document.getElementById('b-001');
 
-input.value = '';
+inputPalindrome.value = '';
 
 const isPalindrome = function (string) {
     let increment = 0;
     let decrement = string.length - 1;
     while (increment < decrement) {
-        if (string[increment] != string[decrement]) {
-            return false;
-        }
+        if (string[increment] != string[decrement]) return false;
         increment++;
         decrement--;
     }
     return true;
 };
 
-button.addEventListener('click', () => {
-    let word = '';
-    if (!isNaN(input.value)) {
+buttonPalindrome.addEventListener('click', () => {
+    if (!isNaN(inputPalindrome.value)) {
         alert('Only string is allowed');
         return;
     }
-    word = input.value;
+    if (isPalindrome(inputPalindrome.value)) alert('The inserted word is palindrome');
+    else alert('The inserted word is NOT palindrome');
 
-    if (isPalindrome(word)) {
-        alert('The inserted word is palindrome');
-    } else {
-        alert('The inserted word is NOT palindrome');
-    }
-    input.value = '';
+    inputPalindrome.value = '';
 });
 
-console.log('js here');
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+const inputOddOrEven = document.getElementById('p-002');
+const buttonOddOrEven = document.getElementById('b-002');
