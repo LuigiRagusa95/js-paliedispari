4a67834f57f46de2cdd87c143faa71d9913576cb
LuigiRagusa95 <luigiragusa99@gmail.com>
2021-10-29 14:47:53 +0200

    add palindrome checker

--- js/main.js
+++ js/main.js
@@ -2,4 +2,38 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
+const input = document.getElementById('p-001');
+const button = document.getElementById('b-001');
+
+input.value = '';
+
+const isPalindrome = function (string) {
+    let increment = 0;
+    let decrement = string.length - 1;
+    while (increment < decrement) {
+        if (string[increment] != string[decrement]) {
+            return false;
+        }
+        increment++;
+        decrement--;
+    }
+    return true;
+};
+
+button.addEventListener('click', () => {
+    let word = '';
+    if (!isNaN(input.value)) {
+        alert('Only string is allowed');
+        return;
+    }
+    word = input.value;
+
+    if (isPalindrome(word)) {
+        alert('The inserted word is palindrome');
+    } else {
+        alert('The inserted word is NOT palindrome');
+    }
+    input.value = '';
+});
+
 console.log('js here');
